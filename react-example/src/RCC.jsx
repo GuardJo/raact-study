6a0827ccdaa21b8fdb90eb8c1843cf9e0f3eff69
kyeongho <dagh1218@naver.com>
2022-06-06 15:36:03 +0900

    리액트 스터디 시작
    Chapter 1 완료

---
+++ react-example/src/RCC.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class RCC extends Component {
+    render() {
+        var text = '따옴표';
+        return <div name="name">{text}</div>;
+    }
+}
+
+RCC.propTypes = {};
+
+export default RCC;
