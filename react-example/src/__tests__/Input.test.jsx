e8dfb5e0b750d132240c5c05d90d178b99d7d012
Kyeongho <kyeongho_jo@lotte.net>
2024-03-25 10:59:53 +0900

    #8 Jest 라이브러리를 활용한 TDD 방식 적용
    - Input 컴포넌트에 대한 테스트 코드 작성

---
+++ react-example/src/__tests__/Input.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Input from '../03/Input';
+
+describe('<Input>', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+
+        ReactDOM.render(<Input name='test'></Input>, div);
+        ReactDOM.unmountComponentAtNode(div);
+
+        expect(React.isValidElement(<Input name='test'></Input>)).toBeTruthy();
+    });
+});
