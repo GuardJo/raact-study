7d6cd0aeb49c57781e0010096fdedc4f46d24c91
Kyeongho <kyeongho_jo@lotte.net>
2024-03-26 10:45:51 +0900

    #14 compose 다중 인자 처리 구현
    - compose 기법을 통해 currying 함수 연산 처리 시 함수의 인자 개수가 복수인 경우 처리 구현
        - ES6의 전개 연산자 사용
        - 혹은, arguments를 나열하여 사용

--- react-example/src/05/compose.js
+++ react-example/src/05/compose.js
@@ -9,18 +9,31 @@ const addFour = add(4);
 // ((x + 4) * 3) * 2
 const formula = x => multiplyTwo(multiplyThree(addFour(x))); 
 
-function compose(funcArr) {
+// function compose() {
+//     const funcArr = Array.prototype.slice.call(arguments);
+
+//     return funcArr.reduce(
+//         function (prevFunc, nextFunc) {
+//             return function(value) {
+//                 const args = Array.prototype.slice.call(arguments);
+//                 return nextFunc(prevFunc.apply(null, args));
+//             };
+//         },
+//         function(k) {return k}
+//     );
+// };
+function compose(...funcArr) {
     return funcArr.reduce(
         function (prevFunc, nextFunc) {
-            return function(value) {
-                return nextFunc(prevFunc(value));
+            return function(...args) {
+                return nextFunc(prevFunc(...args));
             };
         },
         function(k) {return k}
     );
 };
 
-const formulaWithCompose = compose([addFour, multiplyThree, multiplyTwo]);
+const formulaWithCompose = compose(addFour, multiplyThree, multiplyTwo);
 const result1 = formula(10);
 const result2 = formulaWithCompose(10);
 
