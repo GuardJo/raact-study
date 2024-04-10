48df8e5a5fa7688ae820fac4cd28fb1417677881
kyeongho <dagh1218@naver.com>
2024-04-10 18:26:21 +0900

    #37 Action 및 Reducer 구현
    - loading 상태를 변경할 수 있는 Action과 Reducer 구현

--- react-example/src/07/ReduxApp.jsx
+++ react-example/src/07/ReduxApp.jsx
@@ -1,18 +1,37 @@
-import React, { PureComponent } from "react";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
+import React, { PureComponent } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const reducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case 'SET_LOADING': {
+            return {
+                ...state,
+                loading: payload,
+            };
+        }
+        default:
+            return state;
+    }
+};
 
 export default class ReduxApp extends PureComponent {
     store = createStore(
-        state => state,
-        { loading: false, name : "Test 상태"},
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+        reducer,
+        { loading: false, name: 'Test 상태' },
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    );
+
+    componentDidMount() {
+        this.store.dispatch({
+            type: 'SET_LOADING',
+            payload: true,
+        });
+    }
 
     render() {
-        return (
-            <Provider store={this.store}>
-                리덕스 예제
-            </Provider>
-        );
+        return <Provider store={this.store}>리덕스 예제</Provider>;
     }
 }
